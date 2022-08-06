f22807811ed59ef1bb7966445123665452b65460
richardMRa <richete05@gmail.com>
2022-08-06 04:13:07 -0500

    add, ejs view engine and path

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,8 +1,19 @@
-console.log('Hello World')
-
 import express from "express";
+import {dirname, join} from "path";
+import { fileURLToPath } from "url";
+
+
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+//Set ejs view engine and views folder
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//Set get endpoint
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000)
 console.log("Server is listening on port", 3000)
