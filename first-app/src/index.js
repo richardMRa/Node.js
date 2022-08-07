61aa9f7b2b3e17d5790e042ceb070c318439606e
richardMRa <richete05@gmail.com>
2022-08-06 19:12:36 -0500

    independant routes folder, ejs templates, boostrap.

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {dirname, join} from "path";
 import { fileURLToPath } from "url";
-
+import indexRoutes from "./routes/index.js";
 
 
 const app = express();
@@ -12,8 +12,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//Set get endpoint
-app.get('/', (req, res) => res.render('index'))
+//Set index routes and styles
+app.use(indexRoutes)
+
+app.use(express.static(join(__dirname, 'public')))
+
 
 app.listen(3000)
 console.log("Server is listening on port", 3000)
---
+++ first-app/src/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+//Set routes
+router.get('/', (req, res) => res.render('index', { title: 'My First Node Website' }))
+router.get('/about', (req, res) => res.render('about', { title: 'About' }))
+router.get('/contact', (req, res) => res.render('contact', { title: 'Contact' }))
+
+export default router;
